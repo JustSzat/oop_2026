bee253cdfc45d2619881f5ceeee3550e52df83b2
JustSzat <justyna.salek123@gmail.com>
2026-01-03 11:08:08 +0100

    Initial version of the class Czlowiek

---
+++ klasy.py
@@ -0,0 +1,15 @@
+# Klasa = Zamysl, Szablon, Przepis
+
+class Czlowiek:
+    # Istota
+    gatunek = "Homo Sapiens"
+    def __init__(self):
+        # Konstruktor
+        # Akt Istnienia
+        print("Niech powstanie Czlowiek")
+
+# Powstawanie obiektu
+# Gotowanie z przepisu
+
+adam = Czlowiek() # moment powstania czlowieka
+print(adam.gatunek)
