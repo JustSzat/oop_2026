6d9c82f8896f7f470cabf46c967ff9438d1cd984
JustSzat <justyna.salek123@gmail.com>
2026-01-23 14:17:11 +0100

    Dodanie klasy Ksiegowego wraz z metoda, ktora oblicza roczna pensje innego pracownika

--- zad2_klasy.py
+++ zad2_klasy.py
@@ -1,8 +1,9 @@
 class Pracownik:
-    def __init__(self, name, surname, position):
+    def __init__(self, name, surname, position, pensja):
         self.name = name
         self.surname = surname
         self.position = position
+        self.pensja = pensja
 
     def przedstaw_sie(self):
         print(f"Czesc, nazywam sie {self.name} {self.surname} i pracuje na stanowisku {self.position}")
@@ -12,6 +13,13 @@ class Informatyk(Pracownik):
         self.jezyk = jezyk
         print(f"Programuje w {self.jezyk}")
 
+class Ksiegowy(Pracownik):
+    def licz_roczna_pensja(self, osoba):
+        osoba.pensja *= 12
+        print(f"Roczna pensja {osoba.name} wynosi {osoba.pensja}")
 
-p1 = Informatyk("Jan", "Kowalski", "programista")
-p1.programuj("Python")
+
+p1 = Informatyk("Jan", "Kowalski", "programista", 7000)
+p1.programuj("JAVA")
+p2 = Ksiegowy("Ryszard", "Mazurek", "Ksiegowy", 6000)
+p2.licz_roczna_pensja(p1)
